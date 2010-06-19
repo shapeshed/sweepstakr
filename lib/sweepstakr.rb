175cdb46f6a25e8c25e57bc44168f8b9aec9cdf3
George Ornbo <george@shapeshed.com>
2010-06-19 22:49:27 +0100

    adding backports gem

--- lib/sweepstakr.rb
+++ lib/sweepstakr.rb
@@ -5,30 +5,6 @@ require 'sinatra'
 require 'haml'
 require 'backports'
 
-class Array
-   # Shuffle the array
-   def shuffle!
-     n = length
-     for i in 0...n
-       r = Kernel.rand(n-i)+i
-       self[r], self[i] = self[i], self[r]
-     end
-     self
-   end
-
-   # Return a shuffled copy of the array
-   def shuffle
-     dup.shuffle!
-   end
-
-   def cycle(values)
-      self.each_with_index do |o, i| 
-        yield(o, values[i % values.length])
-      end
-    end
-
- end
-
 class Sweepstakr < Sinatra::Application
 
   set :root, APP_ROOT
@@ -44,13 +20,13 @@ class Sweepstakr < Sinatra::Application
     @players = ['george','paul','seb','mike','shaun','vincent','alex','toby']
 
     def sweep_the_stake(things,people)
-
+      
       result = Hash.new { |h,k| h[k] = [] }
 
       things = things.shuffle
       people = people.shuffle
 
-      people_cycle = cycle(people)
+      people_cycle = people.cycle
       things.each do |thing|
          person = people_cycle.next
          result[person] << thing
