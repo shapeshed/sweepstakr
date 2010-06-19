997cd91e582703822acb4d05d4a3abaffb367ec6
George Ornbo <george@shapeshed.com>
2010-06-19 22:31:14 +0100

    come on!

--- lib/sweepstakr.rb
+++ lib/sweepstakr.rb
@@ -12,35 +12,40 @@ class Sweepstakr < Sinatra::Application
 
   get '/' do
     
-    class Array
-       # Shuffle the array
-       def shuffle!
-         n = length
-         for i in 0...n
-           r = Kernel.rand(n-i)+i
-           self[r], self[i] = self[i], self[r]
-         end
-         self
-       end
 
-       # Return a shuffled copy of the array
-       def shuffle
-         dup.shuffle!
-       end
-
-       def cycle(values)
-          self.each_with_index do |o, i| 
-            yield(o, values[i % values.length])
-          end
-        end
-
-     end
      
     @teams = [ 'Algeria', 'England','Korea DPR','Serbia','Argentina','France','Korea Republic','Slovakia','Australia','Germany','Mexico','Slovenia','Brazil','Ghana','Netherlands','South Africa','Cameroon','Greece','New Zealand','Spain','Chile','Honduras','Nigeria','Switzerland','Côte d\'Ivoire','Italy','Paraguay','Uruguay','Denmark','Japan','Portugal','USA']
 
     @players = ['george','paul','seb','mike','shaun','vincent','alex','toby']
 
     def sweep_the_stake(things,people)
+      
+      
+      class Array
+         # Shuffle the array
+         def shuffle!
+           n = length
+           for i in 0...n
+             r = Kernel.rand(n-i)+i
+             self[r], self[i] = self[i], self[r]
+           end
+           self
+         end
+
+         # Return a shuffled copy of the array
+         def shuffle
+           dup.shuffle!
+         end
+
+         def cycle(values)
+            self.each_with_index do |o, i| 
+              yield(o, values[i % values.length])
+            end
+          end
+
+       end      
+      
+      
       result = Hash.new { |h,k| h[k] = [] }
 
       things = things.shuffle
